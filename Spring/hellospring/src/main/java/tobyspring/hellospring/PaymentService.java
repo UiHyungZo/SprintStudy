304d968023b54bd01cea914bab9054aa1acaffbb
UiHyungZo <powerjuh@gmail.com>
2025-03-16 20:09:39 +0900

    인터페이스 도입

--- Spring/hellospring/src/main/java/tobyspring/hellospring/PaymentService.java
+++ Spring/hellospring/src/main/java/tobyspring/hellospring/PaymentService.java
@@ -13,7 +13,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PaymentService {
     //private final WebApiExRateProvider exRateProvider;
-    private final SimpleExRateProvider exRateProvider;
+    private final ExRateProvider exRateProvider;
 
     public PaymentService(){
         this.exRateProvider = new SimpleExRateProvider();
