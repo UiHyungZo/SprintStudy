24250d8b46c102efb8ece1c3b1ad73188f8d0b19
UiHyungZo <powerjuh@gmail.com>
2025-03-16 22:30:00 +0900

    Ioc/DI 적용

--- Spring/hellospring/src/main/java/tobyspring/hellospring/Client.java
+++ Spring/hellospring/src/main/java/tobyspring/hellospring/Client.java
@@ -1,5 +1,8 @@
 package tobyspring.hellospring;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 
@@ -8,8 +11,11 @@ public class Client {
         //PaymentService paymentService = new WebApiExRatePaymentService();
         //PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
 
-        ObjectFactory objectFactory = new ObjectFactory();
-        PaymentService paymentService = objectFactory.paymentService();
+        //ObjectFactory objectFactory = new ObjectFactory();
+        //PaymentService paymentService = objectFactory.paymentService();
+
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(100L,"USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
